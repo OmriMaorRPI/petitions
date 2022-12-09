1e1079d655f11552de1f64487abdf0b1ad87c0e5
OmriMaorRPI <99051303+OmriMaorRPI@users.noreply.github.com>
2022-12-09 09:34:29 -0500

    Undo voting increase and redo workaround

--- client/views/static/api.js
+++ client/views/static/api.js
@@ -6,20 +6,20 @@ Template.api.helpers({
         submitted: 1409767345903,
         title: "Build more affordable, on-campus housing.",
         description: "As there is only enough housing for half the student population, it creates a challenge to find affordably priced off-campus housing.",
-        votes: 350,
+        votes: 50,
         _id: "PYwoRCiAQsWa365af",
         response: "Hi Gabe, thank you for your response. I have reached out to the Director of Housing Operations with your concerns. We will be posting the official response shortly. Thank you. -- Kyle Keraga, RPI Student Senate",
         responded_at: 1411139919817,
-        minimumVotes: 325
+        minimumVotes: 25
       },
       {
         author: "Michael Cuozzo",
         submitted: 1410112945911,
         title: "Extend hours for RPI Computer Labs at peak times.",
         description: "Students often work late near the end of semester; extended lab time will allow more students to utilize this on-campus resource.",
-        votes: 300,
+        votes: 4,
         _id: "bDA8ynBMErm9NLaqj",
-        minimumVotes: 225
+        minimumVotes: 25
       }
     ];
     return JSON.stringify(response, undefined, 2);
@@ -30,7 +30,7 @@ Template.api.helpers({
        submitted: 1410112945911,
        title: "Extend hours for RPI Computer Labs at peak times.",
        description: "Students often work late near the end of semester; extended lab time will allow more students to utilize this on-campus resource.",
-       votes: 41,
+       votes: 4,
        _id: "bDA8ynBMErm9NLaqj",
        minimumVotes: 25,
        signers:[  
@@ -42,17 +42,17 @@ Template.api.helpers({
        history:[  
           {  
              created_at: 1410112945912,
-             votes: 341,
+             votes: 1,
              _id: "ibdMPcGaS5gGmBEha"
           },
           {  
              created_at: 1410199345913,
-             votes: 452,
+             votes: 2,
              _id: "5bw2TeyDK48XcqSiw"
           },
           {  
              created_at: 1410285745914,
-             votes: 564,
+             votes: 4,
              _id: "jZu2D26gwRcZExFy2"
           }
        ]
--- server/fixtures.js
+++ server/fixtures.js
@@ -2,7 +2,7 @@
 
 if (Singleton.find().count() === 0) {
   Singleton.insert({
-    minimumThreshold: 10,
+    minimumThreshold: 50,
     threshold_updated_at: new Date().getTime()
   });
 }
@@ -43,50 +43,50 @@ if (Posts.find().count() === 0 && process.env.NODE_ENV != "production" ) {
   Scores.insert({
     postId: postId_seven_day,
     created_at: moment().subtract(7, 'days').valueOf(),
-    score: 300,
-    votes: 300
+    score: 1,
+    votes: 1
   });
 
   Scores.insert({
     postId: postId_seven_day,
     created_at: moment().subtract(6, 'days').valueOf(),
-    score: 310,
-    votes: 310
+    score: 10,
+    votes: 10
   });
 
   Scores.insert({
     postId: postId_seven_day,
     created_at: moment().subtract(5, 'days').valueOf(),
-    score: 322,
-    votes: 322
+    score: 22,
+    votes: 22
   });
 
   Scores.insert({
     postId: postId_seven_day,
     created_at: moment().subtract(4, 'days').valueOf(),
-    score: 330,
-    votes: 330
+    score: 30,
+    votes: 30
   });
 
   Scores.insert({
     postId: postId_seven_day,
     created_at: moment().subtract(3, 'days').valueOf(),
-    score: 344,
-    votes: 344
+    score: 44,
+    votes: 44
   });
 
   Scores.insert({
     postId: postId_seven_day,
     created_at: moment().subtract(2, 'days').valueOf(),
-    score: 347,
-    votes: 347
+    score: 47,
+    votes: 47
   });
 
   Scores.insert({
     postId: postId_seven_day,
     created_at: moment().subtract(1, 'day').valueOf(),
-    score: 350,
-    votes: 350
+    score: 50,
+    votes: 50
   });
 
   // Post with 3-day history
@@ -98,29 +98,29 @@ if (Posts.find().count() === 0 && process.env.NODE_ENV != "production" ) {
     title: "Extend hours for RIT Computer Labs at peak times.",
     description: "Students often work late near the end of semester; extended lab time will allow more students to utilize this on-campus resource.",
     upvoters: [pete._id],
-    votes: 334,
+    votes: 4,
     minimumVotes: Singleton.findOne().minimumThreshold
   });
 
   Scores.insert({
     postId: postId_three_day,
     created_at: moment().subtract(3, 'days').valueOf(),
-    score: 331,
-    votes: 331
+    score: 1,
+    votes: 1
   });
 
   Scores.insert({
     postId: postId_three_day,
     created_at: moment().subtract(2, 'days').valueOf(),
-    score: 332,
-    votes: 332
+    score: 2,
+    votes: 2
   });
 
   Scores.insert({
     postId: postId_three_day,
     created_at: moment().subtract(1, 'day').valueOf(),
-    score: 334,
-    votes: 334
+    score: 4,
+    votes: 4
   });
 
   // Post with 0-day history
@@ -132,7 +132,7 @@ if (Posts.find().count() === 0 && process.env.NODE_ENV != "production" ) {
     title: "Offer more options for students with unique dietary needs.",
     description: "Increase the number of options for vegan students at on-campus Dining Service locations.",
     upvoters: [pete._id],
-    votes: 331,
+    votes: 1,
     minimumVotes: Singleton.findOne().minimumThreshold
   });
 
@@ -146,7 +146,7 @@ if (Posts.find().count() === 0 && process.env.NODE_ENV != "production" ) {
       title: 'Test post #' + i,
       description: "Foo",
       upvoters: [pete._id],
-      votes: 331,
+      votes: 1,
       minimumVotes: Singleton.findOne().minimumThreshold
     });
   }
