c742bd6f07c7bc4a19efe94ac21c891a2dd79dbb
OmriMaorRPI <99051303+OmriMaorRPI@users.noreply.github.com>
2022-12-09 02:30:04 -0500

    Formatting and postcard meteor code
    Formatting handlebar file
    
    post_card added code to try and induce condition for checkmark to appear

--- client/helpers/handlebars.js
+++ client/helpers/handlebars.js
@@ -1,34 +1,33 @@
-Handlebars.registerHelper('session', function (input) {
+Handlebars.registerHelper("session", function (input) {
   return Session.get(input);
 });
 
-Handlebars.registerHelper('loggedIn', function () {
+Handlebars.registerHelper("loggedIn", function () {
   return Meteor.user() != null;
 });
 
-Handlebars.registerHelper('breaklines', function (text) {
-  text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
+Handlebars.registerHelper("breaklines", function (text) {
+  text = text.replace(/(\r\n|\n|\r)/gm, "<br>");
   return new Handlebars.SafeString(text);
 });
 
-Handlebars.registerHelper('formatDate', function (datetime, format) {
+Handlebars.registerHelper("formatDate", function (datetime, format) {
   if (moment) {
     return moment(datetime).format(format);
-  }
-  else {
+  } else {
     return datetime;
   }
 });
 
-Handlebars.registerHelper('pluralize', function(n, thing) {
+Handlebars.registerHelper("pluralize", function (n, thing) {
   if (n === 1) {
-    return '1 ' + thing;
+    return "1 " + thing;
   } else {
-    return n + ' ' + thing + 's';
+    return n + " " + thing + "s";
   }
 });
 
-Handlebars.registerHelper('empty', function (entity) {
+Handlebars.registerHelper("empty", function (entity) {
   if (typeof entity === "string" || typeof entity === "object") {
     return entity.length == 0;
   } else {
@@ -36,51 +35,52 @@ Handlebars.registerHelper('empty', function (entity) {
   }
 });
 
-Handlebars.registerHelper('selected', function (obj1, obj2) {
+Handlebars.registerHelper("selected", function (obj1, obj2) {
   return obj1 == obj2 ? "selected" : "";
 });
 
-
-Handlebars.registerHelper('equal', function (obj1, obj2) {
+Handlebars.registerHelper("equal", function (obj1, obj2) {
   return obj1 == obj2;
 });
 
-Handlebars.registerHelper('upcase', function (str) {
+Handlebars.registerHelper("upcase", function (str) {
   return str.toUpperCase();
 });
 
-Handlebars.registerHelper('checked', function (obj) {
+Handlebars.registerHelper("checked", function (obj) {
   return obj ? "checked" : "";
 });
 
-Handlebars.registerHelper('fromNow', function (time) {
+Handlebars.registerHelper("fromNow", function (time) {
   var timeTick = new Deps.Dependency();
-  Meteor.setInterval(function () { timeTick.changed(); }, 1000);
+  Meteor.setInterval(function () {
+    timeTick.changed();
+  }, 1000);
   timeTick.depend();
   return new moment(time).fromNow().toUpperCase();
 });
 
-Handlebars.registerHelper('singleton', function () {
+Handlebars.registerHelper("singleton", function () {
   return Singleton.findOne();
 });
 
-Handlebars.registerHelper('tag', function (tag_id) {
-  return Tags.findOne({_id: tag_id});
+Handlebars.registerHelper("tag", function (tag_id) {
+  return Tags.findOne({ _id: tag_id });
 });
 
 var routeUtils = {
-  context: function() {
+  context: function () {
     return Router.current();
   },
-  regex: function(expression) {
-    return new RegExp(expression, 'i');
+  regex: function (expression) {
+    return new RegExp(expression, "i");
   },
-  testRoutes: function(routeNames) {
+  testRoutes: function (routeNames) {
     var reg = this.regex(routeNames);
     return this.context() && reg.test(this.context().route.getName());
-  }
+  },
 };
 
-Handlebars.registerHelper('isActiveRoute', function(route) {
-  return routeUtils.testRoutes(route) ? 'active' : '';
+Handlebars.registerHelper("isActiveRoute", function (route) {
+  return routeUtils.testRoutes(route) ? "active" : "";
 });
